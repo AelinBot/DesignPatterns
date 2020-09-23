04d68699ffa586a8ad0356260085bd7fd2e74e43
R Quiros <cristina.r.quiros@accenture.com>
2020-09-23 17:33:28 +0300

    .gitignore

--- src/bot/aelin/coffeeshop/Barista.java
+++
@@ -1,8 +0,0 @@
-package bot.aelin.coffeeshop;
-
-public class Barista {
-
-    public void update(Object coffeeOrder) {
-        System.out.println("There is a new order: " + coffeeOrder);
-    }
-}
